838290b96e9c1ba63006b18ab22d60ad82b75387
faith ojeabulu <faithojeabulu@gmail.com>
2020-10-16 22:52:39 +0100

    sastry number

--- sastry-numbers.py
+++ sastry-numbers.py
@@ -1,3 +1,25 @@
+"""
+Author:faith ojeabulu 
+Date:10th october 2020 
+
+
+
+Question:In this challenge, you have to establish if a given integer n is a Sastry number. If the number resulting from the concatenation of an integer n with its successor is a perfect square, then n is a Sastry Number.
+
+Given a positive integer n, implement a function that returns True if n is a Sastry number, or False if it's not. 
+
+example:
+is_sastry(106755) ➞ True
+# Concatenation of n and its successor = 106755106756
+# 106755106756 is a perfect square (326734 ^ 2)
+
+
+"""
+
+
+
+
+
 import math
 def is_sastry(n):
     a  = int(str(n) + str(n+1))
