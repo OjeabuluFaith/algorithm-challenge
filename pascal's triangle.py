cb47c8a1dab5df7240055a0346d3dffdfe37ca9a
faith ojeabulu <faithojeabulu@gmail.com>
2020-10-16 21:41:59 +0100

    pascal triangle

--- pascal's triangle.py
+++ pascal's triangle.py
@@ -1,3 +1,16 @@
+"""
+Author:faith ojeabulu 
+Date:10th october 2020 
+
+Goal : The goal of this challenge is to return Pascal’s triangle up to number 29. 
+Pascal’s triangle is the sum of the two upper corners.
+
+Question: Create a function that returns a row from Pascal’s triangle. 
+To find the row and column you can use n!/(k!*(n-k)!) where n is the row down and k is the column. """
+
+
+
+
 def pascals_triangle(n):
     a=1
     b=[1]
