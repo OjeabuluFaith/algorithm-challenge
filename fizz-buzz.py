c9e137f0046627164b05021a702746308e707a0c
faith ojeabulu <faithojeabulu@gmail.com>
2020-10-16 21:39:04 +0100

    fizz buzz

--- fizz-buzz.py
+++ fizz-buzz.py
@@ -1,6 +1,17 @@
-"""Write a program that returns a list of all the numbers from 1 to an integer argument. 
+"""
+Author:faith ojeabulu 
+Date:10th october 2020 
+
+
+
+Question: Write a program that returns a list of all the numbers from 1 to an integer argument. 
 But for multiples of three use “Fizz” instead of the number and for the multiples of five use 
 “Buzz”. For numbers which are multiples of both three and five use “FizzBuzz". """
+
+
+
+
+
 def fizz_buzz(maximum):
     lst =[]
     for i in range(1, maximum + 1):
