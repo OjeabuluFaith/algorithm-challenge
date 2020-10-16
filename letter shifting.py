701f34f7415a806028ffd1736da9a0716349bb44
faith ojeabulu <faithojeabulu@gmail.com>
2020-10-16 22:45:56 +0100

    letter shifting

--- letter shifting.py
+++ letter shifting.py
@@ -1,3 +1,24 @@
+"""
+Author:faith ojeabulu 
+Date:10th october 2020 
+
+
+
+Question: Create a function that takes in a sentence and a character to find. 
+Return a dictionary of each word in the sentence, with the number of the specified character as the value.
+
+example:
+shift_letters("This is a test",  4) ➞ "test Th i sisa"
+
+
+
+"""
+
+
+
+
+
+
 def shift_letters(txt, n):
     letters = txt.replace(' ', '')
     n %= len(letters)
