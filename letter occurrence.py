219c35a132acc461d39b3d58e9523bd933ba52db
faith ojeabulu <faithojeabulu@gmail.com>
2020-10-16 22:20:13 +0100

    letter occurence

--- letter occurrence.py
+++ letter occurrence.py
@@ -1,3 +1,25 @@
+"""
+Author:faith ojeabulu 
+Date:10th october 2020 
+
+
+
+Question: Create a function that takes in a sentence and a character to find. 
+Return a dictionary of each word in the sentence, with the number of the specified character as the value.
+
+
+example: find_occurrences("Hello World", "o") ➞ {
+  "hello" : 1,
+  "world" : 1
+}
+
+"""
+
+
+
+
+
+
 def find_occurrences(txt, ch):
     a = {}
     txt = t.lower()
