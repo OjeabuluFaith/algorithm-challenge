3d500d76cf2c8898535bde51da667b9e47448bf6
faith ojeabulu <faithojeabulu@gmail.com>
2020-10-16 21:47:01 +0100

    tic tac toe

--- tic tac toe.py
+++ tic tac toe.py
@@ -1,3 +1,20 @@
+"""
+Author:faith ojeabulu 
+Date:10th october 2020 
+
+
+
+Question: Create a function that returns a number which can block the player from reaching 3 in a row in a game of Tic Tac Toe. 
+The number given as an argument will correspond to a grid of Tic Tac Toe: topleft is 0, topright is 2, bottomleft is 6, and bottomright is 8.
+
+Steps:
+1)Create a function that takes two numbers a, b and returns another number.
+2)This number should be the final one in a line to block the player from winning.
+
+"""
+
+
+
 def block_player(a, b):
 	lines = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,5,7]]
 	for l in lines:
