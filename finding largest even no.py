afe7d15a2b3287bce20767e5af166297e2129d08
faith ojeabulu <faithojeabulu@gmail.com>
2020-10-16 22:13:52 +0100

    largest even num

--- finding largest even no.py
+++ finding largest even no.py
@@ -1,3 +1,19 @@
+"""
+Author:faith ojeabulu 
+Date:10th october 2020 
+
+
+
+Question: Write a function that finds the largest even number in a list.
+ Return -1 if not found. The use of built-in function max() and sorted() are prohibited.
+
+
+example: ([1, 3, 5, 7]) ➞ -1
+
+"""
+
+
+
 def largest_even(lst):
     res = -1
     for x in lst:
