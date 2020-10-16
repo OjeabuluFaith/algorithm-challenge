f6192c7e00ec196cacec18ef49f42d77b33492c2
faith ojeabulu <faithojeabulu@gmail.com>
2020-10-16 22:49:56 +0100

    multiply by length

--- multiply_by_length.py
+++ multiply_by_length.py
@@ -1,3 +1,23 @@
+"""
+Author:faith ojeabulu 
+Date:10th october 2020 
+
+
+
+Question: Create a function to multiply all of the values in a list by the amount of values in the given list.
+
+
+example :
+MultiplyByLength([4, 1, 1]) ➞ ([12, 3, 3])
+
+MultiplyByLength([1, 0, 3, 3, 7, 2, 1]) ➞  [7, 0, 21, 21, 49, 14, 7]
+
+"""
+
+
+
+
+
 def MultiplyByLength(arr):
     return [i*len(arr) for i in arr]
     
